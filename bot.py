74e7bf6780c7e89a9e8125f9aa0c859cc180cacc
pchchv <ipchchv@gmail.com>
2022-07-24 22:45:00 +0300

    Implement a simple bot

--- bot.py
+++ bot.py
@@ -1,4 +1,16 @@
+import asyncio
+from aiogram import Bot
+
 with open('token.txt', 'r') as t:
     BOT_TOKEN = t.read()
 
-print(BOT_TOKEN)
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+
+    try:
+        me = await bot.get_me()
+        print(f"🤖 Hello, I'm {me.first_name}.\nHave a nice Day!")
+    finally:
+        await bot.close()
+
+asyncio.run(main())
