a57e14abe1c47d68899b84cf30427d84e766ff48
pchchv <ipchchv@gmail.com>
2022-07-25 10:27:41 +0300

    Implement data retrieval from API

--- tgbot.py
+++ tgbot.py
@@ -3,6 +3,8 @@ from aiogram import Bot
 
 with open('token.txt', 'r') as t:
     BOT_TOKEN = t.read()
+t.close()
+
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
@@ -12,5 +14,3 @@ async def main():
         print(f"🤖 Hello, I'm {me.first_name}.\nHave a nice Day!")
     finally:
         await bot.close()
-
-asyncio.run(main())
